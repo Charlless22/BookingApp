daaaf73a24ef080d21b860de1e5e4e1d15b1a1ca
Charles Delachapelle <charles.delachapelle@epitech.eu>
2025-04-13 21:51:41 +0200

    in case commit

--- view/loginView.php
+++ view/loginView.php
@@ -1,8 +1,33 @@
 <!DOCTYPE html>
 <html>
     <header>
+        <title>BookIt</title>
+        <link rel="stylesheet" href="../css/login.css">
+        <link rel="icon" type="image/png" href="../assets/logo.png">
     </header>
     <body>
+        <div class="container">
+            <div class="top-login">
+            </div>
+            <div class="middle-login">
+                <div class="container-login">
+                    <div class="logo-div-login">
+                        <img alt="logo" src="../assets/logo.png" style="width: 200px; height: 200px">
+                    </div>
+                    <form class="form-container" action="dashboardView.php">
+                        <div class="id-container">
+                                <label for="email">Email</label>
+                                <input id="email" type="email" required>
+
+                                <label for="password">Password</label>
+                                <input id="password" type="password" required>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="bottom-login">
+            </div>
+        </div>
     </body>
     <footer>
     </footer>
